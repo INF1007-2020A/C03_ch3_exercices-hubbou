943ca9d2dd3158f59c7337cc910061667c387d33
hubbou <hubert.boucher@live.ca>
2020-09-14 18:27:48 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -14,7 +14,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    # 360 degree = 2pi radians
+    # degree / 360 = radians
     # deg
 
     # Get deg (float)
