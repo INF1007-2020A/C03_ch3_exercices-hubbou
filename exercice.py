bdcbb41ddc4237b77f9b297317cb528fed65a48e
hubbou <hubert.boucher@live.ca>
2020-09-14 18:29:52 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -32,7 +32,7 @@ def to_degrees(angle_rads: float) -> tuple:
 
 
 def to_celsius(temperature: float) -> float:
-    return 0.temperature
+    return temperature
 
 
 def to_farenheit(temperature: float) -> float:
